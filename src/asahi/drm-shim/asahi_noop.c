0fac710d18ee883682884eb70531090e7c9b24c6
Asahi Lina <lina@asahilina.net>
2022-11-08 10:04:59 -0500

    asahi: Extend noop drm-shim for the new UAPI

--- src/asahi/drm-shim/asahi_noop.c
+++ src/asahi/drm-shim/asahi_noop.c
@@ -23,11 +23,121 @@
  */
 
 #include "drm-shim/drm_shim.h"
+#include "drm-uapi/asahi_drm.h"
+#include "asahi/lib/agx_pack.h"
 
 bool drm_shim_driver_prefers_first_render_node = true;
 
+struct asahi_bo {
+   struct shim_bo base;
+   uint32_t offset;
+};
+
+static struct asahi_bo *
+asahi_bo(struct shim_bo *bo)
+{
+   return (struct asahi_bo *)bo;
+}
+
+struct asahi_device {
+   uint64_t next_offset;
+};
+
+static struct asahi_device asahi = {
+   .next_offset = 0x1000,
+};
+
+static int
+asahi_ioctl_noop(int fd, unsigned long request, void *arg)
+{
+   return 0;
+}
+
+static int
+asahi_ioctl_submit(int fd, unsigned long request, void *arg)
+{
+   struct drm_asahi_submit *submit = arg;
+   const void *cmdbuf = (void *) (uintptr_t) submit->cmdbuf;
+
+   struct AGX_IOGPU_HEADER header;
+
+   AGX_IOGPU_HEADER_unpack(stderr, cmdbuf, &header);
+
+   printf("Encoder: %" PRIx64 "\n", header.encoder);
+
+   // Lina: Your code goes here!
+
+   return 0;
+}
+
+static int
+asahi_ioctl_create_bo(int fd, unsigned long request, void *arg)
+{
+   struct shim_fd *shim_fd = drm_shim_fd_lookup(fd);
+   struct drm_asahi_create_bo *create = arg;
+   struct asahi_bo *bo = calloc(1, sizeof(*bo));
+
+   drm_shim_bo_init(&bo->base, create->size);
+
+   assert(UINT64_MAX - asahi.next_offset > create->size);
+   bo->offset = asahi.next_offset;
+   asahi.next_offset += create->size;
+
+   create->offset = bo->offset;
+   create->handle = drm_shim_bo_get_handle(shim_fd, &bo->base);
+
+   drm_shim_bo_put(&bo->base);
+
+   return 0;
+}
+
+static int
+asahi_ioctl_get_bo_offset(int fd, unsigned long request, void *arg)
+{
+   struct shim_fd *shim_fd = drm_shim_fd_lookup(fd);
+   struct drm_asahi_get_bo_offset *args = arg;
+   struct shim_bo *bo = drm_shim_bo_lookup(shim_fd, args->handle);
+
+   args->offset = asahi_bo(bo)->offset;
+
+   drm_shim_bo_put(bo);
+
+   return 0;
+}
+
+static int
+asahi_ioctl_mmap_bo(int fd, unsigned long request, void *arg)
+{
+   struct shim_fd *shim_fd = drm_shim_fd_lookup(fd);
+   struct drm_asahi_mmap_bo *map = arg;
+   struct shim_bo *bo = drm_shim_bo_lookup(shim_fd, map->handle);
+
+   map->offset = drm_shim_bo_get_mmap_offset(shim_fd, bo);
+
+   drm_shim_bo_put(bo);
+
+   return 0;
+}
+
+static int
+asahi_ioctl_get_param(int fd, unsigned long request, void *arg)
+{
+   struct drm_asahi_get_param *gp = arg;
+
+   switch (gp->param) {
+      default:
+         fprintf(stderr, "Unknown DRM_IOCTL_ASAHI_GET_PARAM %d\n", gp->param);
+         return -1;
+   }
+}
+
 static ioctl_fn_t driver_ioctls[] = {
-   /* The Asahi Linux UAPI is not yet upstream */
+   [DRM_ASAHI_SUBMIT] = asahi_ioctl_submit,
+   [DRM_ASAHI_WAIT] = asahi_ioctl_noop,
+   [DRM_ASAHI_CREATE_BO] = asahi_ioctl_create_bo,
+   [DRM_ASAHI_MMAP_BO] = asahi_ioctl_mmap_bo,
+   [DRM_ASAHI_GET_PARAM] = asahi_ioctl_get_param,
+   [DRM_ASAHI_GET_BO_OFFSET] = asahi_ioctl_get_bo_offset,
 };
 
 void
